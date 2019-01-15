11d00ab0d7730e7bd7102b4cb3148ad1047731a9
MichaelRol <michael.rollins@hotmail.co.uk>
2019-01-15 14:22:59 +0000

    Cleaned up 43

--- Problems 41-50/43-SubstringDivisibility.py
+++ Problems 41-50/43-SubstringDivisibility.py
@@ -2,16 +2,6 @@ import itertools
 
 pans = list(itertools.permutations('1234567890'))
 
-# divisors = [2, 3, 5, 7, 11, 13, 17]
-# sum = 0
-
-# def isPandigital(val):
-#     string = str(val)
-#     if '1' in string and '2' in string and '3' in string and '4' in string and '5' in string \
-#         and '6' in string and '7' in string and '8' in string and '9' in string:
-#         return True
-#     return False
-
 sum = 0 
 for pan in pans:
     panstr = ''.join(pan)
