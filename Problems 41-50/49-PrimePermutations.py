b59a09188c09074dba1d78007c2b869dd8f1e14d
MichaelRol <michael.rollins@hotmail.co.uk>
2019-01-15 02:18:31 +0000

    problem 49 I feel im close, but no there yet

--- Problems 41-50/49-PrimePermutations.py
+++ Problems 41-50/49-PrimePermutations.py
@@ -3,7 +3,7 @@ def isPrime(x):
         return False
     if x == 2:
         return True
-    for i in range(3,int(x**0.5)+1, 2):
+    for i in range(2,int(x**0.5)+1):
         if x % i == 0:
             return False
     return True
@@ -13,10 +13,25 @@ for x in range(1001, 10000, 2):
     if isPrime(x):
         primes.append(x)
 
+def arePerms(val, add):
+    strval = str(val)
+    strval1 = str(val + x)
+    strval2 = str(val + 2*x)
+
+    for dig in range(0, len(strval)-1):
+        if strval[dig] in strval1 and strval[dig] in strval2:
+            strval1 = strval1.replace(strval[dig], '')
+            strval2 = strval2.replace(strval[dig], '')
+        else:
+            return False
+    if strval1 == '':            
+        return True
+    return False
 
 for prime in primes:
     for x in range(1, 10000):
         if prime + x*2 > 10000:
             break
-        if isPrime(prime + x) and isPrime(prime + 2*x):
-            print(str(prime)+str(prime+x)+str(prime+2*x)) 
+        if arePerms(prime, x):
+            if isPrime(prime + x) and isPrime(prime + 2*x):
+                print(str(prime)+str(prime+x)+str(prime+2*x)) 
