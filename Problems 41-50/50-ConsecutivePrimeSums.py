469fea297d26e375f85821425cc0fb99e2b60f51
MichaelRol <michael.rollins@hotmail.co.uk>
2019-01-15 01:31:15 +0000

    Problem 50 Completed super fast

--- Problems 41-50/50-ConsecutivePrimeSums.py
+++ Problems 41-50/50-ConsecutivePrimeSums.py
@@ -3,31 +3,53 @@ def isPrime(x):
         return False
     if x == 2:
         return True
-    for i in range(3,int(x**0.5)+1,2):
+    for i in range(2,int(x**0.5)+1):
         if x % i == 0:
             return False
     return True
 
-primes = [2]
+primeSums = [2]
 for x in range(3, 1000000, 2):
+    if x + primeSums[len(primeSums) - 1] > 1000000:
+         break
     if isPrime(x):
-        primes.append(x)
+        primeSums.append(x + primeSums[len(primeSums) - 1])
 
+longestumVal = 0
 longestSumLength = 0
-longestSumVal = 0
-for goal in range(len(primes) -1, 50000, -1):
+
+for goal in range(len(primeSums) - 1, 0, -1):
     for start in range(0, goal):
-        sum = 0
-        if goal - start < longestSumLength:
-            break
-        for prime in range(start, goal - 1):
-            sum = sum + primes[prime]
-            if sum == primes[goal] and prime - start > longestSumLength:
-                longestSumLength = prime - start
-                longestSumVal = sum
+        if goal - start > longestSumLength:
+            if isPrime(primeSums[goal] - primeSums[start]):
+                longestSumLength = goal - start
+                longestumVal = primeSums[goal] - primeSums[start]
                 break
-            if sum > primes[goal]:
-                break
-    print(longestSumVal)
-    print(longestSumLength)
+
+print(longestumVal)
+
+
+# def calcLongestSum(goal, longestSum):
+#     for start in range(0, goal):
+#         sum = 0
+#         if goal - start < longestSum[0]:
+#             break
+#         for prime in range(start, goal - 1):
+#             sum = sum + primeSums[prime]
+#             if sum == primeSums[goal] and prime - start > longestSum[0]:
+#                 longestSum[0] = prime - start
+#                 longestSum[1] = sum
+#                 return longestSum
+#             if sum > primeSums[goal]:
+#                 break
+#     return 
+
+# longestSum = [0, 0]
+# for goal in range(len(primeSums) -1, 0, -1):
+#     hold = calcLongestSum(goal, longestSum)
+#     if hold[1] > longestSum[1]:
+#         longestSum = hold
+#         print(longestSum)
+
+    
         
