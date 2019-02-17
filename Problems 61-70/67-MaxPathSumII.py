f1b87c986cb5fd032b26e682520ad0dbb533fe56
MichaelRol <michael.rollins@hotmail.co.uk>
2019-02-17 20:19:20 +0000

    Parsed file into 2-d array

--- Problems 61-70/67-MaxPathSumII.py
+++ Problems 61-70/67-MaxPathSumII.py
@@ -2,6 +2,12 @@ array = []
 
 file = open("p067_triangle.txt", "r")
 for line in file:
-    array.append(line)
+    rowstring = line.replace("\n", "")
+    rowstring = line.split(" ")
+    row = []
+    for val in rowstring:
+        row.append(int(val))
+
+    array.append(row)
 
 print(array)
