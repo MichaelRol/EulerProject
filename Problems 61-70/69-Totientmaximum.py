31303c27dadf3fb497c6ff8faa9e8c684fb91fb4
MichaelRol <michael.rollins@hotmail.co.uk>
2019-02-17 23:02:48 +0000

    Im fairly sure 69 is logically correct but is far too slow

--- Problems 61-70/69-Totientmaximum.py
+++ Problems 61-70/69-Totientmaximum.py
@@ -1,23 +1,27 @@
-def isPrime(x):
-    if x < 2 :
-        return False
-    if x == 2:
-        return True
-    for i in range(3,int(x**0.5)+1, 2):
-        if x % i == 0:
-            return False
-    return True
-
-def genPrimes(x):
-    primes = [2]
-    numOfPrimes = 1
-    num = 3
-    while numOfPrimes <= x:
-        if isPrime(num):
-            numOfPrimes += 1
-            primes.append(num)
-        num += 2
-    return primes
+def gcd(a, b):
+    if a == 0:
+        return b
+    return gcd(b % a, a)
 
 def phi(n):
-    primes2n = genPrimes(n)
+    count = 1
+    for x in range(2, n):
+        if gcd(n, x) == 1:
+            count += 1
+    return count
+
+max = 0
+value = 0
+count = 0
+for n in range(1000000, 0, -1):
+    nphin = float(n)/float(phi(n))
+    if nphin > max:
+        count = 0
+        max = nphin
+        value = n
+    else:
+        count += 1
+    if count > 100:
+        break
+
+print(n)
