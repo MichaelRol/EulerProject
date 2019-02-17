60e8021033c989c77f603ffaf76ee3a295decaea
MichaelRol <michael.rollins@hotmail.co.uk>
2019-02-17 17:34:30 +0000

    Problem 60 working for example 4 primes, now need to get working for 5

--- Problems 51-60/60-PrimePairSets.py
+++ Problems 51-60/60-PrimePairSets.py
@@ -19,16 +19,20 @@ def genPrimes(x):
         num += 2
     return primes
 
-count = 0
-primes = genPrimes(100)
+lowest = 1000000000000
+primes = genPrimes(150)
 for one in primes:
     for two in primes:
-        if one != two:
+        if one != two and isPrime(int(str(one) + str(two))) and isPrime(int(str(two) + str(one))):
             for three in primes:
-                if one != three and two != three:
+                if one != three and two != three and isPrime(int(str(one) + str(three))) and isPrime(int(str(two) + str(three)))\
+                    and isPrime(int(str(three) + str(one))) and isPrime(int(str(three) + str(two))):
                     for four in primes:
-                        if one != four and two != four and three != four:
-                            for five in primes:
-                                if one != five and two != five and three != five and four != five:
-                                    count += 1
-print(count)
+                        if one != four and two != four and three != four and two != three and isPrime(int(str(one) + str(four))) and isPrime(int(str(two) + str(four)))\
+                            and isPrime(int(str(four) + str(one))) and isPrime(int(str(four) + str(two))) and isPrime(int(str(four) + str(three))) and isPrime(int(str(three) + str(four))):
+                            if one + two + three + four < lowest:
+                                lowest = one + two + three + four
+                            # for five in primes:
+                            #     if one != five and two != five and three != five and four != five:
+                            #         count += 1
+print(lowest)
