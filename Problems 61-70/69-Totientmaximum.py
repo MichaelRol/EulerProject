3d7cb969fa6459221b854094ab1347520c89c44d
MichaelRol <michael.rollins@hotmail.co.uk>
2019-02-18 00:10:32 +0000

    Problem 69 completed - Phi function written by Nikita Tiwari from https://www.geeksforgeeks.org/eulers-totient-function/

--- Problems 61-70/69-Totientmaximum.py
+++ Problems 61-70/69-Totientmaximum.py
@@ -1,27 +1,25 @@
-def gcd(a, b):
-    if a == 0:
-        return b
-    return gcd(b % a, a)
 
-def phi(n):
-    count = 1
-    for x in range(2, n):
-        if gcd(n, x) == 1:
-            count += 1
-    return count
+def phi(n): 
+    result = n  
+    p = 2
+    while p * p<= n:
+        if  n % p == 0: 
+            while n % p == 0: 
+                n = n // p 
+            result = result * (1.0 - (1.0 / (float) (p))) 
+        p = p + 1
+    if n > 1: 
+        result = result * (1.0 - (1.0 / (float)(n))) 
+   
+    return (int)(result) 
 
 max = 0
 value = 0
-count = 0
-for n in range(1000000, 0, -1):
+for n in range(2, 1000000):
     nphin = float(n)/float(phi(n))
     if nphin > max:
         count = 0
         max = nphin
         value = n
-    else:
-        count += 1
-    if count > 100:
-        break
 
-print(n)
+print(value)
