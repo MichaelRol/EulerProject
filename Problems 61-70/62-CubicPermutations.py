a154ce272ff433a5fccd795389c65e5ef720015e
MichaelRol <michael.rollins@hotmail.co.uk>
2019-02-23 17:10:06 +0000

    Tried some other things, still far too slow

--- Problems 61-70/62-CubicPermutations.py
+++ Problems 61-70/62-CubicPermutations.py
@@ -1,14 +1,29 @@
 import itertools
 
-for x in range(0, 1000):
+def genCubes(num):
+    cubes = []
+    for x in range(1, num+1):
+        cubes.append(list(str(x**3)))
+    return cubes
+
+# cubes = genCubes(500)
+for x in range(1000, 1100):
     cubed = x**3
     perms = list(itertools.permutations(str(cubed)))
     count = []
+    # compare = 1
+    # while (len(str(compare**3)) <= len(str(cubed))):
+    #     if list(str(compare**3)) in perms:
+    #         count.append(compare**3)
+    #     compare += 1
     for perm in perms:
-        rooted = int("".join(perm))**(1.0/3)
+        strperm = "".join(perm)
+        rooted = int(strperm)**(1.0/3)
         rooted = round(rooted)
-        if int(rooted**3) == int("".join(perm)):
-            if "".join(perm) not in count and perm[0] != "0":
-                count.append("".join(perm))
-    if len(count) == 4:
+        # if perm in cubes:
+        #     count.append("".join(perm))
+        if int(rooted**3) == int(strperm):
+            if strperm not in count and perm[0] != "0":
+                count.append(strperm)
+    if len(count) == 5:
         print(x**3, count)
