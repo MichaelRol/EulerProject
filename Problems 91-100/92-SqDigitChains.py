2d78567261a0532b86331b504c94d38687eba8c9
Michael Rollins <michael.rollins@hotmail.co.uk>
2019-10-15 22:21:50 +0100

    Problem 92 Complete but 40 seconds too slow

--- Problems 91-100/92-SqDigitChains.py
+++ Problems 91-100/92-SqDigitChains.py
@@ -1,1 +1,15 @@
-//TODO 
+count = 0
+for x in range(2, 10000000):
+    sum = x
+    while(1):
+        digits = [int(d) for d in str(sum)]
+        sum = 0
+        for num in digits:
+            sum += num*num
+        y = sum
+        if sum == 89:
+            count += 1
+            break
+        if sum == 1:
+            break
+print(count)
