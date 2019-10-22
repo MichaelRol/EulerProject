e49043e22f0772870c4a52d49b620762a11fa084
Michael Rollins <michael.rollins@hotmail.co.uk>
2019-10-23 00:54:50 +0100

    Problem 92 Completed

--- Problems 91-100/92-SqDigitChains.py
+++ Problems 91-100/92-SqDigitChains.py
@@ -1,15 +1,36 @@
+import itertools
+import math
+
+
+def multinomcoef(number):
+    numDigits = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(0, len(number)):
+        numDigits[number[i]] += 1
+
+    result = math.factorial(len(number))
+    for i in range(0, len(numDigits)):
+        result /= math.factorial(numDigits[i])
+    return result
+
+nums = list(itertools.combinations_with_replacement("0123456789", 7))
 count = 0
-for x in range(2, 10000000):
-    sum = x
+numbers = 0
+for x in nums:
+    sum = int(''.join(x))
+    if sum == 0:
+        continue
     while(1):
         digits = [int(d) for d in str(sum)]
         sum = 0
         for num in digits:
             sum += num*num
-        y = sum
         if sum == 89:
-            count += 1
+            toSend = [int(d) for d in x]
+            count += multinomcoef(toSend)
+            numbers += 1
             break
         if sum == 1:
             break
 print(count)
+
+#TRY LIST OF COMBINATIONS
