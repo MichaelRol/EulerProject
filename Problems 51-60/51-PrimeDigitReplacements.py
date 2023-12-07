ccacbbdfd5520871bfc27224b347c5933fe43eed
Michael Rollins <MichaelRol@users.noreply.github.com>
2023-12-07 22:53:00 +0000

    Problem 51 Completed

--- Problems 51-60/51-PrimeDigitReplacements.py
+++ Problems 51-60/51-PrimeDigitReplacements.py
@@ -1,25 +1,38 @@
+import itertools
+
+prime_cache = set()
+
 def isPrime(x):
+    if (x in prime_cache):
+        return x
     if x < 2 :
         return False
     if x == 2:
+        prime_cache.add(x)
         return True
     for i in range(2,int(x**0.5)+1):
         if x % i == 0:
             return False
+    prime_cache.add(x)
     return True
 
-for num in range(11, 99, 2):
-    if isPrime(num):
-        for dig in range(0, len(str(num))):
+def findPrimeReplacementFamily(length):
+    for num in range(99999, 9999999, 2):
+        if isPrime(num):
+            indices = range(len(str(num)))
             numstr = list(str(num))
-            primes = 0
-            for x in range(0, 10):
-                curstr = numstr
-                curstr[dig] = str(x)
-                if isPrime(int(''.join(curstr))):
-                    primes += 1
-            if primes >= 6:
-                print(num)
-            
-            
+            for num_indices_to_replace in indices[1:]:
+                for indices_to_replace in itertools.combinations(indices, num_indices_to_replace):
+                    curstr = numstr.copy()
+                    primes = []
+                    for x in range(0, 10):
+                        for dig in indices_to_replace:
+                            curstr[dig] = str(x)
+                        constructed_num = int(''.join(curstr))
+                        if isPrime(constructed_num) and len(str(num)) == len(str(constructed_num)):
+                            primes += [constructed_num]
+                        if len(primes) >= length:
+                            return primes
+                
+print(findPrimeReplacementFamily(8))
             
