99c4fac25f14a16eeabff0a180a6ecd72fa052d5
MichaelRol <michael.rollins@hotmail.co.uk>
2023-12-11 22:55:13 +0000

    Problem 76: Speed enhancement, still not good enough

--- Problems 71-80/76-CountingSummations.py
+++ Problems 71-80/76-CountingSummations.py
@@ -1,6 +1,5 @@
 from math import floor
 
-
 def findSummations(num):
     if (num == 2):
         return [[1, 1]]
@@ -8,17 +7,14 @@ def findSummations(num):
     newSums = []
     for prevSum in prevSums:
         newSums.append(prevSum + [1])
-    for newSum in newSums:
+    for newSum in newSums.copy():
         for k in range(2, floor(num/2) + 1):
-            allEndInone = True
-            for x in range(2, k + 1):
-                allEndInone = allEndInone and newSum[-x] == 1
-            if allEndInone:
+            if newSum.count(1) == k:
                 sumCopy = newSum.copy()
-                for _ in range(k):
-                    sumCopy.remove(1)
+                del sumCopy[-k:]
                 sumCopy.append(k)
-                sumCopy.sort(reverse=True)
+                if k != 2:
+                    sumCopy.sort(reverse=True)
                 if sumCopy not in newSums:
                     newSums.append(sumCopy)
     if [num - 1, 1] not in newSums:
