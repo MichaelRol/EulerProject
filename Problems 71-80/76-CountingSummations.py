621eb6e28871fad7b3c00c9db3739e0df9a50f9c
MichaelRol <michael.rollins@hotmail.co.uk>
2023-12-12 16:40:34 +0000

    Problem 76: Runtime for input 60 halved, still can't run 100

--- Problems 71-80/76-CountingSummations.py
+++ Problems 71-80/76-CountingSummations.py
@@ -3,26 +3,23 @@ import time
 
 def findSummations(num):
     if (num == 2):
-        return set([(1, 1)])
+        return {(1, 1) : 2}
     prevSums = findSummations(num-1)
-    newSums = set()
+    newSums = dict()
     for prevSum in prevSums:
-        newSums.add(prevSum + (1,))
-    for newSum in newSums.copy():
+        newSums[prevSum + (1,)] = prevSums[prevSum] + 1
+    for newSum, sumLen in newSums.copy().items():
         for k in range(2, int(num/2) + 1):
-            sumLen = len(newSum)
             if sumLen > k and newSum[-k] == 1 and newSum[-(k+1)] != 1:
                 newSumTuple = newSum[:sumLen-k] + (k,)
                 if k != 2:
                     newSumTuple = tuple(sorted(newSumTuple, reverse=True))
-                if newSumTuple not in newSums:
-                   newSums.add(newSumTuple)
-    if (num - 1, 1) not in newSums:
-        newSums.add((num - 1, 1))
+                newSums[newSumTuple] = sumLen - k + 1
+    newSums[(num - 1, 1)] = 2
     return newSums
 
 t0 = time.time()
-sums = findSummations(70)
+sums = findSummations(100)
 t1 = time.time()
 
 print(t1-t0)
