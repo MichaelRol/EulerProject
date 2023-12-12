f9d214b1ed413cf217fe0f98773098172b7ae19a
MichaelRol <michael.rollins@hotmail.co.uk>
2023-12-12 13:33:24 +0000

    Problem 76: Futher performance enhancement

--- Problems 71-80/76-CountingSummations.py
+++ Problems 71-80/76-CountingSummations.py
@@ -9,17 +9,20 @@ def findSummations(num):
     for prevSum in prevSums:
         newSums.add(prevSum + (1,))
     for newSum in newSums.copy():
-        for k in range(2, floor(num/2) + 1):
-            if newSum.count(1) == k:
-                newSumTuple = newSum[:len(newSum)-k] + (k,)
+        for k in range(2, int(num/2) + 1):
+            sumLen = len(newSum)
+            if sumLen > k and newSum[-k] == 1 and newSum[-(k+1)] != 1:
+                newSumTuple = newSum[:sumLen-k] + (k,)
                 if k != 2:
                     newSumTuple = tuple(sorted(newSumTuple, reverse=True))
-                newSums.add(newSumTuple)
-    newSums.add((num - 1, 1))
+                if newSumTuple not in newSums:
+                   newSums.add(newSumTuple)
+    if (num - 1, 1) not in newSums:
+        newSums.add((num - 1, 1))
     return newSums
 
 t0 = time.time()
-sums = findSummations(100)
+sums = findSummations(70)
 t1 = time.time()
 
 print(t1-t0)
